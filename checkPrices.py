ec01ab212b5284f070f8d25365b46ed79fa039b4
Dimitris Kalogeropoulos <dmtrs_kal@hotmail.com>
2019-11-22 19:40:26 +0200

    Fix output for prices >= 1000

--- checkPrices.py
+++ checkPrices.py
@@ -72,7 +72,7 @@ try:
 				
 				#Parse Price
 				price = price_box.text.strip() # strip() is used to remove starting and trailing
-				price = price.replace(".","")
+				price = price.replace(".","")  # fix prices >= 1000
 				str_price = re.findall("\d+\,\d+", price) # Price with , as floating point
 				str_price = str_price[0]
 				
